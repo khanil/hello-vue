b98e2a93227ad3fd3af965949320d87e526a05d6
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2019-05-24 16:19:05 +0300

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Welcome!</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium mollitia culpa fugit,
+      consectetur dolores perspiciatis modi eum ex ipsam ratione similique sequi hic cumque
+      exercitationem voluptate expedita, quam enim porro ipsa eaque esse. Tempore, autem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum libero expedita cumque
+      ipsum voluptate ex sed vitae consequatur ipsa quod fugit, blanditiis, at eum adipisci optio?
+      Recusandae nulla mollitia magnam veritatis explicabo sit cum labore. Ea laboriosam odio,
+      obcaecati in dignissimos aliquam est delectus id ut esse dicta quo? Sed deserunt, repellat
+      odit magni quisquam assumenda itaque velit illum labore odio libero molestiae? Quos, dicta
+      suscipit itaque illo quasi dolorem, doloribus omnis repellendus explicabo deserunt fuga,
+      accusamus ipsum ab? Assumenda doloribus ipsum saepe fugiat, soluta, laudantium, incidunt
+      facilis consectetur illum deleniti aliquid ad repellat quasi et iure quis laborum veritatis.
+    </p>
   </div>
 </template>
 
