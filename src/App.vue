38cd7713889f2d94b6265a20da22d98e07c1af54
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2019-05-24 16:18:42 +0300

    add global styles

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,80 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: $font-family-default;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $color;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+ul {
+  display: flex;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  color: $link-color;
+  text-decoration: $link-text-decoration;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: $link-hover-color;
   }
+
+  &:focus,
+  &:active {
+    outline: none;
+  }
+}
+
+input,
+button {
+  padding: $input-btn-padding-y $input-btn-padding-x;
+  font-size: $input-btn-font-size;
+  line-height: $input-btn-line-height;
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+}
+
+input {
+  border: $input-border;
+  color: $input-color;
+
+  &:active,
+  &:focus {
+    box-shadow: $input-focus-box-shadow;
+  }
+}
+
+button {
+  border: $btn-border;
+  background-color: $btn-bg-color;
+  color: $btn-color;
+
+  &:active,
+  &:focus {
+    box-shadow: $btn-focus-box-shadow;
+  }
+}
+
+#app {
+  margin: 0 auto 20px;
+  padding: 0 20px;
+  max-width: 768px;
 }
 </style>
